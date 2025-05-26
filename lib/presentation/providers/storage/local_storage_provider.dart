ed0aea4e98617d181a8d08aafac9b206ee6593cd
Mikko Piaggio <mikkopk48@gmail.com>
2025-05-26 13:29:11 -0300

    Refactor IsarDatasource implementation; move to infrastructure layer and update methods

--- lib/presentation/providers/storage/local_storage_provider.dart
+++ lib/presentation/providers/storage/local_storage_provider.dart
@@ -1,8 +1,7 @@
-
-import 'package:cinemapedia/domain/datasources/isar_datasource.dart';
 import 'package:cinemapedia/domain/repositories/local_storage_respository_impl.dart';
+import 'package:cinemapedia/infrastructure/datasources/isar_datasource.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final localStorageRepositoryProvider = Provider((ref){
-return LocalStorageRespositoryImpl(IsarDatasource());
+final localStorageRepositoryProvider = Provider((ref) {
+  return LocalStorageRespositoryImpl(IsarDatasource());
 });
